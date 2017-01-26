485e281fc8228eec61c4f6484ab009e1053c4e57
mateuszroth <mateusz.roth@gmail.com>
2017-01-26 14:27:38 +0100

    home security system

--- device/client.js
+++ device/client.js
@@ -1,45 +1,116 @@
-var EtherPortClient = require("etherport-client").EtherPortClient;
-var five = require("johnny-five");
-var board, led;
+const five = require('johnny-five');
+const EtherPortClient = require('etherport-client').EtherPortClient;
+const socket = require('socket.io-client')('http://192.168.43.113:8080'); // #TODO update server ip
 
-var board = new five.Board({
+console.log('Preparing...');
+
+const board = new five.Board({
   port: new EtherPortClient({
-    host: "192.168.1.113", // #TODO update esp device ip
+    host: '192.168.43.111', // #TODO update esp device ip
     port: 3030
   }),
   timeout: 1e5,
   repl: false
 });
 
-board.on("ready", function() {
-  console.log("READY!");
+console.log('Almost done, please wait 5 seconds...');
 
-  // init a led on pin 2, strobe every 1000ms
-  led = new five.Led(2).strobe(1000);
+let armored = 0;
+let emitted = 0;
 
-  // setup a standard servo, center at start
-  servo = new five.Servo({
-    pin:6,
-    range: [0,180],
-    type: "standard",
-    center:true
-  });
+// differences between D1R1 and D1R2
+// https://forum.wemos.cc/uploads/files/1460120067677-comparison-of-wemos-r2-vs-r1-pinouts.png
 
-  // poll this sensor every second
-  sensor = new five.Sensor({
-    pin: "A0",
-    freq: 1000
-  });
+// differences between Arduino UNO and D1R2
+// http://www.instructables.com/id/Programming-the-WeMos-Using-Arduino-SoftwareIDE/
+// http://www.instructables.com/file/FNU5UQRIMTE9CWV/
+
+board.on('connect', function() {
+
+    board.log('Board', 'Connected...');
+
 });
 
-var socket = require('socket.io-client')('http://192.168.1.102:8080'); // #TODO update server ip
+board.on('ready', function() {
+
+  board.log('Board', 'Ready!');
+
+  this.pinMode(2, this.MODES.OUTPUT);
+  this.pinMode(12, this.MODES.OUTPUT); // GPIO12 is D6/D12
+  this.pinMode(13, this.MODES.INPUT); // GPIO13 is D7/D11
+
+  board.digitalWrite(2, 0);
+  board.digitalWrite(12, 0);
+
+  this.loop(150, function() {
+  // Whatever the last value was, write the opposite
+
+    if(armored) {
+
+      board.digitalWrite(12, 0);
+
+      board.digitalRead(13, function(detected) {
+
+          if(detected && armored) {
+
+              board.digitalWrite(12, 1);
+
+              if(!emitted) {
+
+                socket.emit('detected', { detected: 'true' });
+                emitted = 1;
+
+              }
+
+          } else if(emitted) {
+
+            board.digitalWrite(12, 1);
+
+          } else {
+
+            board.digitalWrite(12, 0);
+            emitted = 0;
+
+          }
+
+      });
+
+    } else {
+
+      board.digitalWrite(12, 0);
+      emitted = 0;
+
+    }
+
+  });
+
+});
+
+board.on('exit', function() {
+
+  board.log('Board', 'Closing...');
+
+  board.digitalWrite(2, 0);
+  board.digitalWrite(12, 0);
+
+});
+
+// SOCKETS
+
 socket.on('news', function (data) {
+
   console.log(data);
+
 });
 
-socket.on('ledSignal', function (data) {
+socket.on('armSignal', function (data) {
+
   console.log(data);
+
   if(board.isReady) {
-    led.strobe(data.delay);
+
+    armored = data.arm;
+
   }
+
 });
--- server/server.js
+++ server/server.js
@@ -1,6 +1,35 @@
 var app = require('http').createServer(handler);
+var http = require('https');
+var querystring = require('querystring');
 var io = require('socket.io').listen(app);
 var fs = require('fs');
+
+// sms config
+var appKey = ''; // #TODO appKey from SMSLabs.pl
+var secretKEy = ''; // #TODO appKey from SMSLabs.pl
+var auth = 'Basic ' + new Buffer(appKey + ':' + secretKEy).toString('base64');
+
+// sms config
+var smsData = querystring.stringify({
+    'flash': '0',
+    'expiration': '0',
+    'phone_number': '+48512345678', // #TODO your phone number
+    'sender_id': 'SMS INFO',
+    'message': 'JESUS CHRIST! SOMEONE IS IN YOUR HOUSE!'
+});
+
+// sms config
+var options = {
+    port: 443,
+    host: 'panel.smslabs.net.pl',
+    path: '/apiSms/sendSms',
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': smsData.length,
+        'Authorization': auth
+    }
+};
 
 // make web server listen on port 8080
 app.listen(8080);
@@ -26,10 +55,22 @@ io.sockets.on('connection', function (socket) {
   socket.emit('news', { connected: 'true' });
 
   // if led message received
-  socket.on('ledWeb', function (data) {
-    var delay = data.delay;
-    console.log(delay);
-    io.sockets.emit('ledSignal',{delay});
+  socket.on('arm', function (data) {
+    var arm = data.arm;
+    console.log('Arm: ' + arm);
+    io.sockets.emit('armSignal',{arm});
+  });
+
+  socket.on('detected', function (data) {
+    var detected = data.detected;
+    console.log('Detected: ' + detected);
+    io.sockets.emit('detected',{detected});
+
+    // send SMS #TODO uncomment
+    var sms = http.request(options, function(res){});
+    sms.on( 'error', function(e){ console.log( e.message ); });
+    sms.write(smsData);
+    sms.end();
   });
 
 });
